3f11a18ea776647c39b46bab92ba56aed4e4179d
Destiny Ezenwata <ezenwatairoegbu97@gmail.com>
2025-04-26 14:53:47 +0100

    verification first set

--- src/app/(auth)/verification.tsx
+++ src/app/(auth)/verification.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import {KeyboardAvoidingView, ScrollView, Platform, View, Image} from "react-native";
+import React, {useState} from 'react';
+import {KeyboardAvoidingView, ScrollView, Platform, View, Text, Image} from "react-native";
 import {ThemedText} from "@/components/ThemedText";
 import {ThemedView} from "@/components/ThemedView";
+import { CodeField, Cursor, useBlurOnFulfill, useClearByFocusCell } from 'react-native-confirmation-code-field';
 
 
 function Verification() {
+    const [value, setValue] = useState('');
+    const ref = useBlurOnFulfill({value, cellCount: 6});
+    const [props, getCellOnLayoutHandler] = useClearByFocusCell({
+        value,
+        setValue,
+    });
+
     return (
         <KeyboardAvoidingView
             style={{flex: 1}}
@@ -16,7 +24,7 @@ function Verification() {
                 contentContainerStyle={{flexGrow: 1}}
                 keyboardShouldPersistTaps="handled"
             >
-                <ThemedView className='flex flex-col flex-grow items-center'>
+                <ThemedView className='flex flex-col flex-grow items-center px-4'>
                     <View className='my-12'>
                         <Image
                             source={require('@/assets/auth/verification-illustraion.png')}
@@ -28,6 +36,30 @@ function Verification() {
                     <ThemedText className='w-[288px] text-xs leading-[13px] text-center opacity-70 font-montserrat-medium mt-2 mb-8'>
                         Enter the 4-digit verification code that was sent to your school email that ends in @mouau.edu.ng attached with your matric number
                     </ThemedText>
+
+                    {/*Code input here*/}
+                    <View className='w-full'>
+                        <CodeField
+                            ref={ref}
+                            {...props}
+                            value={value}
+                            onChangeText={setValue}
+                            cellCount={4}
+                            keyboardType="number-pad"
+                            textContentType="oneTimeCode" // enables auto-fill
+                            renderCell={({index, symbol, isFocused}) => (
+                                <ThemedView
+                                    key={index}
+                                    className="flex-1 h-24 border-2 rounded-md mx-2 border-gray-300 items-center justify-center"
+                                    style={isFocused && {borderColor: '#000'}}
+                                    onLayout={getCellOnLayoutHandler(index)}>
+                                    <ThemedText className="text-xl font-montserrat-medium">
+                                        {symbol || (isFocused ? <Cursor /> : null)}
+                                    </ThemedText>
+                                </ThemedView>
+                            )}
+                        />
+                    </View>
                 </ThemedView>
             </ScrollView>
         </KeyboardAvoidingView>
