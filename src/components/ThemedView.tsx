a1f01d90ab1307b99f42b0e7205aaa7dd599cdb6
Destiny Ezenwata <ezenwatairoegbu97@gmail.com>
2025-04-27 17:19:17 +0100

    using tailwind for ThemedView.tsx

--- src/components/ThemedView.tsx
+++ src/components/ThemedView.tsx
@@ -1,16 +1,10 @@
-import { View, type ViewProps } from 'react-native';
+import {View, type ViewProps} from 'react-native';
 
-import { useThemeColor } from '@/core/hooks/useThemeColor';
-import {useTheme} from "@/core/contexts/ThemeContext";
 
 export type ThemedViewProps = ViewProps & {
-  lightColor?: string;
-  darkColor?: string;
+	className?: string;
 };
 
-export function ThemedView({ style, lightColor, darkColor, ...otherProps }: ThemedViewProps) {
-  useTheme()
-  const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
-
-  return <View style={[{ backgroundColor }, style]} {...otherProps} />;
+export function ThemedView({className = '', ...rest}: ThemedViewProps) {
+	return <View className={` bg-background-light dark:bg-background-dark ${className}`} {...rest} />;
 }
