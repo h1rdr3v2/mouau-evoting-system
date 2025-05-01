796a90de0d13d65d0ad5dcd89994e55f0a9a4fba
Destiny Ezenwata <ezenwatairoegbu97@gmail.com>
2025-05-01 14:50:47 +0100

    added safeareaview in settings.tsx

--- src/app/(protected)/(tabs)/settings.tsx
+++ src/app/(protected)/(tabs)/settings.tsx
@@ -2,25 +2,28 @@ import React from 'react';
 import {useAuth} from "@/core/queries/useAuth";
 import {ThemedView} from "@/components/ThemedView";
 import {ThemedText} from "@/components/ThemedText";
+import {ThemeToggle} from "@/components/ThemeToggle";
 import {ThemedButton} from "@/components/ThemedButton";
 import {useAuthStore} from "@/core/stores/useAuthStore";
-import {ThemeToggle} from "@/components/ThemeToggle";
+import {ThemedSafeAreaView} from "@/components/ThemedSafeAreaView";
 
 function SettingsScreen() {
 	const user = useAuthStore(state => state.user);
 	const {logout} = useAuth();
 	
 	return (
-		<ThemedView className='flex-1'>
-			<ThemedText>Tabs index</ThemedText>
-			<ThemedText>User: {JSON.stringify(user)}</ThemedText>
-			<ThemedButton
-				title="Logout"
-				variant='destructive'
-				onPress={() => logout()}
-			/>
-			<ThemeToggle/>
-		</ThemedView>
+		<ThemedSafeAreaView>
+			<ThemedView className='flex-1'>
+				<ThemedText>Tabs index</ThemedText>
+				<ThemedText>User: {JSON.stringify(user)}</ThemedText>
+				<ThemedButton
+					title="Logout"
+					variant='destructive'
+					onPress={() => logout()}
+				/>
+				<ThemeToggle/>
+			</ThemedView>
+		</ThemedSafeAreaView>
 	);
 }
 
