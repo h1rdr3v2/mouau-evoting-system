fb9364fcfcef4a7d6dd1c602669a0ccdf1d73d01
Destiny Ezenwata <ezenwatairoegbu97@gmail.com>
2025-05-20 11:39:14 +0100

    adding safe area view in 2fa screen

--- src/app/(auth)/verification.tsx
+++ src/app/(auth)/verification.tsx
@@ -6,6 +6,7 @@ import {ThemedView} from "@/components/ThemedView";
 import {ThemedButton} from "@/components/ThemedButton";
 import {CodeField, Cursor, useBlurOnFulfill, useClearByFocusCell} from 'react-native-confirmation-code-field';
 import {KeyboardAvoidingView, ScrollView, Platform, View, Image, TextInput, Alert, Pressable} from "react-native";
+import {ThemedSafeAreaView} from "@/components/ThemedSafeAreaView";
 
 const CELL_COUNT = 4;
 
@@ -32,77 +33,79 @@ function VerificationScreen() {
 		<KeyboardAvoidingView
 			style={{flex: 1}}
 			behavior={Platform.OS === "ios" ? "padding" : "height"}
-			keyboardVerticalOffset={Platform.OS === "ios" ? 100 : 20}
+			keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 20}
 		>
-			<ScrollView
-				style={{flex: 1}}
-				contentContainerStyle={{flexGrow: 1}}
-				showsVerticalScrollIndicator={false}
-				keyboardShouldPersistTaps="handled"
-			>
-				<ThemedView className='flex flex-col flex-grow items-center px-4'>
-					<View className='my-12'>
-						<Image
-							source={require('@/assets/auth/verification-illustraion.png')}
-						/>
-					</View>
-					<ThemedText type='title'>
-						Verification Code
-					</ThemedText>
-					<ThemedText
-						className='w-[288px] text-xs leading-[13px] text-center opacity-70 font-montserrat-medium mt-2 mb-8'>
-						Enter the 4-digit verification code that was sent to your school email that ends in
-						@mouau.edu.ng attached with your matric number
-					</ThemedText>
-					
-					{/*Code input here*/}
-					<View className='w-full mb-8'>
-						<CodeField
-							ref={ref}
-							{...props}
-							value={value}
-							onChangeText={setValue}
-							cellCount={CELL_COUNT}
-							keyboardType="number-pad"
-							textContentType="oneTimeCode" // enables auto-fill
-							autoComplete={(Platform.select({
-								android: 'sms-otp',
-								default: 'one-time-code'
-							})) as 'sms-otp' | 'one-time-code'}
-							testID="verification-code-input"
-							renderCell={({index, symbol, isFocused}) => (
-								<ThemedView
-									key={index}
-									className="flex-1 h-24 border-2 rounded-md mx-2 border-gray-300 items-center justify-center"
-									style={isFocused && {borderColor: '#000'}}
-									onLayout={getCellOnLayoutHandler(index)}>
-									<TextInput className="text-3xl font-montserrat-medium dark:text-text-dark">
-										{symbol || (isFocused ? <Cursor/> : null)}
-									</TextInput>
-								</ThemedView>
-							)}
-						/>
-						<View className='flex flex-row items-center gap-1 self-center pt-6'>
-							<ThemedText>Didn’t get the code?</ThemedText>
-							<Pressable>
-								<ThemedText
-									className="font-montserrat-semibold dark:text-white text-primary-light text-lg">
-									Resend
-								</ThemedText>
-							</Pressable>
+			<ThemedSafeAreaView>
+				<ScrollView
+					style={{flex: 1}}
+					contentContainerStyle={{flexGrow: 1}}
+					showsVerticalScrollIndicator={false}
+					keyboardShouldPersistTaps="handled"
+				>
+					<ThemedView className='flex flex-col flex-grow items-center px-4 my-12'>
+						<View>
+							<Image
+								source={require('@/assets/auth/verification-illustraion.png')}
+							/>
 						</View>
-					</View>
-					
-					{/*Continue button*/}
-					<ThemedButton
-						title='Continue'
-						size='large'
-						className='w-full'
-						onPress={handleFullLogin}
-						disabled={value.length < CELL_COUNT}
-					/>
-				</ThemedView>
-			</ScrollView>
+						<ThemedText type='title'>
+							Verification Code
+						</ThemedText>
+						<ThemedText
+							className='w-[288px] text-xs leading-[13px] text-center opacity-70 font-montserrat-medium mt-2 mb-8'>
+							Enter the 4-digit verification code that was sent to your school email that ends in
+							@mouau.edu.ng attached with your matric number
+						</ThemedText>
+						
+						{/*Code input here*/}
+						<View className='w-full mb-8'>
+							<CodeField
+								ref={ref}
+								{...props}
+								value={value}
+								onChangeText={setValue}
+								cellCount={CELL_COUNT}
+								keyboardType="number-pad"
+								textContentType="oneTimeCode" // enables auto-fill
+								autoComplete={(Platform.select({
+									android: 'sms-otp',
+									default: 'one-time-code'
+								})) as 'sms-otp' | 'one-time-code'}
+								testID="verification-code-input"
+								renderCell={({index, symbol, isFocused}) => (
+									<ThemedView
+										key={index}
+										className="flex-1 h-24 border-2 rounded-md mx-2 border-gray-300 items-center justify-center"
+										style={isFocused && {borderColor: '#000'}}
+										onLayout={getCellOnLayoutHandler(index)}>
+										<TextInput className="text-3xl font-montserrat-medium dark:text-text-dark">
+											{symbol || (isFocused ? <Cursor/> : null)}
+										</TextInput>
+									</ThemedView>
+								)}
+							/>
+							<View className='flex flex-row items-center gap-1 self-center pt-6'>
+								<ThemedText>Didn’t get the code?</ThemedText>
+								<Pressable>
+									<ThemedText
+										className="font-montserrat-semibold dark:text-white text-primary-light text-lg">
+										Resend
+									</ThemedText>
+								</Pressable>
+							</View>
+						</View>
+						
+						{/*Continue button*/}
+						<ThemedButton
+							title='Continue'
+							size='large'
+							className='w-full'
+							onPress={handleFullLogin}
+							disabled={value.length < CELL_COUNT}
+						/>
+					</ThemedView>
+				</ScrollView>
+			</ThemedSafeAreaView>
 		</KeyboardAvoidingView>
 	);
 }
