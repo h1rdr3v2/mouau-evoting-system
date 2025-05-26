c95627aee55a8512ccdb11b7e30a8c70d1deaa68
Destiny Ezenwata <ezenwatairoegbu97@gmail.com>
2025-05-26 10:53:30 +0100

    minor change: using different safe area view

--- src/components/ThemedSafeAreaView.tsx
+++ src/components/ThemedSafeAreaView.tsx
@@ -1,5 +1,5 @@
-import {SafeAreaView, type ViewProps} from 'react-native';
-
+import {type ViewProps} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 export type ThemedViewProps = ViewProps & {
 	className?: string;
