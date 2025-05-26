9efcc95ddbcb46672bffcb398df226d29815e3ed
Destiny Ezenwata <ezenwatairoegbu97@gmail.com>
2025-05-26 11:50:21 +0100

    minor change: using rn safeareaview

--- src/components/ThemedSafeAreaView.tsx
+++ src/components/ThemedSafeAreaView.tsx
@@ -1,10 +1,10 @@
-import {type ViewProps} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context';
+import {SafeAreaView, type ViewProps} from 'react-native';
 
 export type ThemedViewProps = ViewProps & {
 	className?: string;
 };
 
 export function ThemedSafeAreaView({className = '', ...rest}: ThemedViewProps) {
-	return <SafeAreaView className={` flex-1 bg-background-light dark:bg-background-dark ${className}`} {...rest} />;
+	return <SafeAreaView
+		className={` flex-1 bg-background-light dark:bg-background-dark border border-red-500 ${className}`} {...rest} />;
 }
